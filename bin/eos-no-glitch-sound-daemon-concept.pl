1ea5ff8c5dc639e31c8f104c00d92b3769a75ab2
Myoungjin Jeon <jeongoon@gmail.com>
2017-01-31 00:22:06 +1100

    fix handling signals: install them after 2nd fork()

--- bin/eos-no-glitch-sound-daemon-concept.pl
+++ bin/eos-no-glitch-sound-daemon-concept.pl
@@ -16,22 +16,28 @@ use File::Which;
 use Fcntl ':flock';
 use Time::HiRes qw(sleep);
 use File::Which;
-use sigtrap handler => handleSigInt => 'INT';
-use sigtrap handler => handleSigErr => 'untrapped';
+
 
 use FindBin;
 use File::Spec;
+our @BaseDir_;
+BEGIN {
+    my ( $volume, $dir, $not_used )
+      = File::Spec->splitpath( $FindBin::RealBin );
+    push @BaseDir_, $volume;
+    push @BaseDir_, File::Spec->splitdir( $dir );
 
-use lib ( map { Cwd::abs_path( File::Spec->catfile( $FindBin::RealBin, @$_ ) ) }
-          ( [ qw(.. lib) ], [ ( '..' ) x (3), 'myPerl' ] ) );
+    require lib;
+    lib->import( map { Cwd::abs_path( File::Spec->catfile ( @BaseDir_, @$_ ) ) }
+                 ( [ 'lib' ], [ ( '..' ) x (1), 'myPerl' ] ) );
+}
 
 use FreeDE::App
-  ( -AppName => 'no-glich-sound',
+  ( -AppName => 'no-glitch-sound',
     -Debug   => true,
     -Verbose => true,
     qw(fs app :msg)
   );
-
 
 package DaemonData;
 use Class::Struct;
@@ -42,6 +48,7 @@ package main;
 sub checkChildren;
 sub isPlayerAlive;
 sub stopPlayer;
+sub reapChildren;
 sub waitSignal;
 
 use version 0.77; our $VERSION = version->declare( 'v0.0.1' );
@@ -50,9 +57,7 @@ info getAppName()." starts normally: version: $VERSION, process number: $$";
 my $daemon = DaemonData->new;
 dmsg "First fork for starting new session(daemon session)";
 
-$daemon->PID( fork );
-# XXX: not sure about return value
-defined $daemon->PID or leave "Could not fork";
+defined $daemon->PID( fork ) or leave $daemon->PID or leave "Could not fork";
 
 if ( $daemon->PID != 0 ) {
     info "start a new daemon: exit";
@@ -60,6 +65,8 @@ if ( $daemon->PID != 0 ) {
 }
 
 POSIX::setsid() or leave "setsid() failed: unexpected error: $!";
+
+# XXX: use sigtrap not working after fork()
 
 spawn_audio_player:
 dmsg "Second fork() for an audio player";
@@ -72,7 +79,7 @@ if ( not defined $daemon->playerPID ) {
 $daemon->keepRunning( true );
 
 if ( $daemon->playerPID == 0 ) {
-    my $no_sound_file = fs->catfile( app->myHome, 'GoogleDrive', 'Audio', 'test.wav' );
+    my $no_sound_file = fs->catfile( @BaseDir_, qw(lib nosound.5sec.wav) );
     dmsg "use $no_sound_file to play for opening sound device.";
 #    close STDIN;
 #    close STDOUT;
@@ -85,6 +92,13 @@ if ( $daemon->playerPID == 0 ) {
 
 $|++;
 dmsg "fork() for player is succeed.";
+dmsg "installing signal handlers...";
+
+#$sigtrap::Verbose = 1;
+require sigtrap;
+sigtrap->import( handler => \&handleSigInt => qw(INT HUP) );
+sigtrap->import( handler => \&handleSigQuit => 'untrapped' );
+
 dmsg "wait for 2 sec: this will be enough for player to start playing";
 sleep 2;
 my $count_sigstop = kill POSIX::SIGSTOP => $daemon->playerPID;
@@ -98,12 +112,13 @@ $daemon->keepRunning and goto manage_daemon;
 
 sub waitSignal {
     sleep 1000 while true;
+    
     checkChildren();
 }
 
 sub checkChildren {
     dmsg "reaping child process";
-    true while waitpid( -1, WNOHANG ) > 0;
+    reapChildren;
     # ref: http://perldoc.perl.org/perlipc.html#Signals
     # FIXME: move to FreeDe::App::pid_is_alive() or something like that.
     if ( not ( kill 0 => $daemon->playerPID or $!{EPERM} ) ) {
@@ -116,11 +131,16 @@ sub handleSigInt () {
     dmsg "FIXME: kill previous player and make a new one";
 }
 
-sub handleSigErr () {
+sub reapChildren () {
+    1 while waitpid( -1 , WNOHANG ) > 0;
+}
+
+sub handleSigQuit () {
     my $cnt_sig;
     dmsg "handling other signals except SIGINT";
     stopPlayer;
-    exit 100;
+    reapChildren;
+    $daemon->keepRunning( false );
 }
 
 sub isPlayerAlive () {
